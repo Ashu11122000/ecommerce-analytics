55f5923de21b1144e054880bf051414fe4780a90
Ashish Sharma <ashu11vats@gmail.com>
2026-09-08 13:48:51 +0530

    Updated Gold layer

--- ecommerce_dbt/models/gold/dimensions/dim_customers.sql
+++ ecommerce_dbt/models/gold/dimensions/dim_customers.sql
@@ -2,9 +2,31 @@ SELECT
     customer_id,
     customer_name,
     email,
+    gender,
+    age_group,
     city,
+    state,
+    country,
+    postal_code,
+    customer_segment,
+    customer_status_group,
     signup_date,
+    customer_age,
+    customer_tenure_days,
+    is_active_customer,
 
+    -- Customer lifecycle classification
+    CASE
+        WHEN customer_tenure_days < 90
+            THEN 'New Customer'
+
+        WHEN customer_tenure_days < 365
+            THEN 'Established Customer'
+
+        ELSE 'Long-Term Customer'
+    END AS customer_lifecycle_stage,
+
+    -- Technical lineage timestamps
     ingested_at,
     loaded_at,
     transformed_at,
--- ecommerce_dbt/models/gold/facts/fact_order_items.sql
+++ ecommerce_dbt/models/gold/facts/fact_order_items.sql
@@ -6,17 +6,57 @@
 }}
 
 SELECT
+    -- Fact grain
     oi.order_item_id,
+
+    -- Dimension keys
     oi.order_id,
     o.customer_id,
     oi.product_id,
+    TO_CHAR(o.order_date::DATE, 'YYYYMMDD')::INTEGER AS order_date_key,
+
+    -- Transaction attributes
     o.order_date,
+    o.order_status,
+    o.sales_channel,
+    o.currency,
+
+    -- Product transaction values
     oi.quantity,
     oi.unit_price,
+    oi.discount_amount,
+    oi.tax_amount,
 
-    oi.quantity * oi.unit_price AS total_amount,
+    -- Source line total
+    oi.line_total,
 
-    -- Data lineage timestamps
+    -- Silver-calculated line total
+    oi.calculated_line_total,
+
+    -- Difference between source and calculated value
+    oi.line_total_variance,
+
+    -- Data-quality flags
+    oi.is_line_total_mismatch,
+    oi.has_negative_quantity,
+
+    -- Return information
+    oi.returned_quantity,
+    oi.has_return,
+    oi.return_rate,
+
+    -- Net sales amount
+    (
+        oi.quantity * oi.unit_price
+        - oi.discount_amount
+        + oi.tax_amount
+    ) AS net_sales_amount,
+
+    -- Gross sales before discount
+    oi.quantity * oi.unit_price AS gross_sales_amount,
+
+    -- Technical lineage timestamps
+    oi.updated_at,
     oi.ingested_at,
     oi.loaded_at,
     oi.transformed_at,
