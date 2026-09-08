322c4e94b13a9357ffbbc9b94798c7d1274ecbbc
Ashish Sharma <ashu11vats@gmail.com>
2026-09-08 10:46:45 +0530

    Updated create_database.sql and create_raw_table.sql

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,3 +1,11 @@
--- Database Creation Script
--- Create the project database
+-- This database will contain:
+--   1. RAW source tables
+--   2. dbt Bronze models
+--   3. dbt Silver models
+--   4. dbt Gold analytical models
+
+-- NOTE:
+-- This script only creates the database.
+-- Schema and table creation are handled separately.
+
 CREATE DATABASE ecommerce_analytics;
