1a2cc0f308271af5673218107d527baa69f30053
Ashish Sharma <ashu11vats@gmail.com>
2026-09-08 17:03:34 +0530

    deleted test_positive_order_amount.sql

--- ecommerce_dbt/tests/test_positive_order_amount.sql
+++
@@ -1,9 +0,0 @@
--- Custom data quality test:
--- Returns rows only when an order item has a non-positive total amount.
-
-SELECT
-    order_item_id,
-    order_id,
-    total_amount
-FROM {{ ref('fact_order_items') }}
-WHERE total_amount <= 0
